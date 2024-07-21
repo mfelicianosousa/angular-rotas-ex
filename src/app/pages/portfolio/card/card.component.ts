28dcb43155ff4abc082adc63fc40cd30c33a16ff
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2024-07-21 07:48:59 -0400

    Angular-Rotas-Exercicio

---
+++ src/app/pages/portfolio/card/card.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent implements OnInit {
+
+  constructor(private activeRoute: ActivatedRoute, private navegador: Router){
+    // http://localhost:4200/portfolio/{1}
+    this.activeRoute.params.subscribe(
+      res => console.log(res)
+    )
+    // http://localhost:4200/portfolio/1?{name=marcelino&token=123}
+     this.activeRoute.queryParams.subscribe(
+      res => console.log(res)
+    )
+  }
+
+  ngOnInit(): void {
+    /*setInterval(() => {
+       this.navegador.navigate(['/'])
+    },5000)
+    */
+  }
+
+
+
+}
